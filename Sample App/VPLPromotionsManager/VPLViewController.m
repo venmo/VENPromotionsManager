5dd165dcea47648f1e3280f3f63130b45655f037
Dasmer Singh <dasmer.singh@venmo.com>
2014-03-04 20:36:05 -0500

    Uncomment VPLViewcontroller implementation

--- Sample App/VPLPromotionsManager/VPLViewController.m
+++ Sample App/VPLPromotionsManager/VPLViewController.m
@@ -6,43 +6,43 @@
 static NSString *kVENPromotionAppleKey = @"ApplePromotionKey";
 
 @implementation VPLViewController
-//
-//- (void)viewDidAppear:(BOOL)animated {
-//    [super viewDidAppear:animated];
-//    CLLocation *appleHQLocation = [[CLLocation alloc] initWithLatitude:37.3318 longitude:-122.0312];
-//    
-//    NSMutableArray *promotions = [[NSMutableArray alloc] init];
-//    
-//    for (NSInteger i=0; i<15; i++) {
-//        NSString *userDefaultsKey = [NSString stringWithFormat:@"%@%d", kVENPromotionAppleKey,i];
-//        VPLPromotion *promotion = [[VPLPromotion alloc] initWithCenter:appleHQLocation
-//                                                                  range:3000
-//                                                              startDate:nil
-//                                                                endDate:nil
-//                                                showOnceUserDefaultsKey:userDefaultsKey
-//                                                                 action:^{
-//                                                                     NSLog(@"Promotion Number %d Fired",(i+1));
-//                                                                 }];
-//        [promotions addObject:promotion];
-//    }
-//    
-//    [VPLPromotionsManager startWithPromotions:[promotions copy]
-//                                locationTypes:VPLLocationTypeGPSRequestPermission
-//                              locationService:nil
-//                          withRefreshInterval:5 withMultipleTriggerType:VPLMultipleTriggerOnRefreshTypeTriggerOnce];
-//}
-//
-//
-//- (IBAction)startStopClicked:(id)sender {
-//    if ([VPLPromotionsManager sharedManager].isRunning) {
-//        [[VPLPromotionsManager sharedManager] stopMonitoringForPromotionLocations];
-//        NSLog(@"Manager Stopped");
-//    }
-//    else {
-//        [[VPLPromotionsManager sharedManager] startMonitoringForPromotionLocations];
-//        NSLog(@"Manager Started Again2");
-//
-//    }
-//}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    CLLocation *appleHQLocation = [[CLLocation alloc] initWithLatitude:37.3318 longitude:-122.0312];
+    
+    NSMutableArray *promotions = [[NSMutableArray alloc] init];
+    
+    for (NSInteger i=0; i<15; i++) {
+        NSString *userDefaultsKey = [NSString stringWithFormat:@"%@%d", kVENPromotionAppleKey,i];
+        VPLPromotion *promotion = [[VPLPromotion alloc] initWithCenter:appleHQLocation
+                                                                  range:3000
+                                                              startDate:nil
+                                                                endDate:nil
+                                                showOnceUserDefaultsKey:userDefaultsKey
+                                                                 action:^{
+                                                                     NSLog(@"Promotion Number %d Fired",(i+1));
+                                                                 }];
+        [promotions addObject:promotion];
+    }
+    
+    [VPLPromotionsManager startWithPromotions:[promotions copy]
+                                locationTypes:VPLLocationTypeGPSRequestPermission
+                              locationService:nil
+                          withRefreshInterval:5 withMultipleTriggerType:VPLMultipleTriggerOnRefreshTypeTriggerOnce];
+}
+
+
+- (IBAction)startStopClicked:(id)sender {
+    if ([VPLPromotionsManager sharedManager].isRunning) {
+        [[VPLPromotionsManager sharedManager] stopMonitoringForPromotionLocations];
+        NSLog(@"Manager Stopped");
+    }
+    else {
+        [[VPLPromotionsManager sharedManager] startMonitoringForPromotionLocations];
+        NSLog(@"Manager Started Again2");
+
+    }
+}
 
 @end
